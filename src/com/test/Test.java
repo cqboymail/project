80203c80b05bacce859e4b484870738d8cd56b69
cqboymail <cqboymail@163.com>
2015-08-02 14:46:31 +0800

    commit

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,8 +4,7 @@ package com.test;
  * Created by cqboy on 2015/8/2.
  */
 public class Test {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("good");
     }
 }
---
+++ src/com/test/Test2.java
@@ -0,0 +1,7 @@
+package com.test;
+
+/**
+ * Created by cqboy on 2015/8/2.
+ */
+public class Test2 {
+}
